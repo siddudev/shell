2eaabf7bc638d7009fdbe98101d6e05dd17d1c12
tiger <siddardha@gmail.com>
2024-08-28 13:40:22 +0530

    script files

---
+++ Script1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#this is a sample script file
+echo "This is the current user"
+whoami
+echo
+echo "files present here"
+ls
+echo
+echo "current working directory"
+pwd
+echo
+echo "files with permissions"
+ls -l
+
+
+
+
+
+
+echo
+echo "content in script file"
+cat Script1.sh
